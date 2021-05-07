0feb494023a79e8c86ef779ff65412ad8408edb1
Dorgan <dorgandash@gmail.com>
2021-05-07 23:10:24 +0000

    Add comments

--- lib/normalizer.ex
+++ lib/normalizer.ex
@@ -35,17 +35,21 @@ defmodule Normalizer do
 
     Macro.prewalk(quoted, fn
       {:__aliases__, _, _} = node ->
+        # Skip aliases, otherwise the module segments will be wrapped in blocks
         node
 
       {:., _, [_, :{}]} = node ->
+        # Skip left hand side of qualified tuple calls
         node
 
       {form, meta, args} = node when is_list(args) ->
         cond do
           meta[:literal?] ->
+            # Skip if it's a literal block
             node
 
           Keyword.has_key?(meta, :do) ->
+            # The call has a do block
             {last_arg, leading_args} = List.pop_at(args, -1)
 
             {_, _, last_arg} = maybe_normalize_literal(last_arg, meta)
@@ -53,20 +57,30 @@ defmodule Normalizer do
             {form, meta, leading_args ++ last_arg}
 
           true ->
+            # For any other call
             args = Enum.map(args, &maybe_normalize_literal(&1, meta))
 
             {form, meta, args}
         end
 
       node ->
+        # Skip every other block
         node
     end)
   end
 
   # Strings
   defp maybe_normalize_literal(x, parent_meta) when is_binary(x) do
-    {:__block__,
-     [literal?: true, line: parent_meta[:line], token: Macro.to_string(x), delimiter: ~s["]], [x]}
+    meta = [
+      literal?: true,
+      line: parent_meta[:line],
+      token: Macro.to_string(x),
+      # There is no way to know if it's a single line string or
+      # a multiline string, so we default to a single line one
+      delimiter: ~s["]
+    ]
+
+    {:__block__, meta, [x]}
   end
 
   # Integers, floats, atoms
@@ -105,28 +119,26 @@ defmodule Normalizer do
   defp maybe_normalize_literal(x, parent_meta) when is_list(x) do
     cond do
       keyword?(x) ->
+        # If it's a keyword list, we need to add `format: :keyword` to the left
+        # hand side metadata, otherwise the keyword list would be printed in its
+        # desugared form [{:foo, :bar}]
         meta = [literal?: true, line: parent_meta[:line], closing: [line: parent_meta[:line]]]
 
-        {:__block__, meta,
-         [
-           Enum.map(x, fn
-             {left, right} ->
-               left_parent_meta =
-                 if is_atom(left) and not Keyword.has_key?(parent_meta, :do) do
-                   Keyword.put(parent_meta, :format, :keyword)
-                 else
-                   meta
-                 end
+        args =
+          Enum.map(x, fn {left, right} ->
+            left_parent_meta = Keyword.put(parent_meta, :format, :keyword)
 
-               {maybe_normalize_literal(left, left_parent_meta),
-                maybe_normalize_literal(right, parent_meta)}
-           end)
-         ]}
+            {maybe_normalize_literal(left, left_parent_meta),
+             maybe_normalize_literal(right, parent_meta)}
+          end)
+
+        {:__block__, meta, [args]}
 
       true ->
-        {:__block__,
-         [literal?: true, line: parent_meta[:line], closing: [line: parent_meta[:line]]],
-         [Enum.map(x, &maybe_normalize_literal(&1, parent_meta))]}
+        meta = [literal?: true, line: parent_meta[:line], closing: [line: parent_meta[:line]]]
+        args = Enum.map(x, &maybe_normalize_literal(&1, parent_meta))
+
+        {:__block__, meta, [args]}
     end
   end
 
